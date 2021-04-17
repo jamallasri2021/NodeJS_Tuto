b86b62855644462e2b94a364896e2fc671d63cbe
Jamal LASRI <jamallasri@hotmail.fr>
2021-04-17 12:55:29 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,51 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let session = require('express-session');
+
+// Template engine
+app.set('view engine', 'ejs');
+
+//#region  Middleware
+// Resources
+app.use('/assets', express.static('public'));
+
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//#endregion
+
+//#region session
+app.use(session({
+    secret: 'cryptoMessage',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+//#endregion
+
+//#region Routes
+app.use(require('./middlewares/flash'))
+
+app.get('/', (request, response) => {
+    // if(request.session.error){
+    //     response.locals.msgError = request.session.error;
+    //     request.session.error = undefined;
+    // }
+    console.log(request.session);
+    response.render('pages/index', { test: 'Edouard' });
+});
+
+app.post('/', (request, response) => {
+    let msg = request.body.message;
+    request.flash('error', "You didn't type a message");
+    if (msg === undefined || msg === '') {
+        //response.render('pages/index', { error: "You didn't type a message :(" });
+        response.redirect('/');
+    } else{
+        
+    }
+});
+//#endregion
+
+app.listen(8080);
